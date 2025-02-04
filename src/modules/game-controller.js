3c84bce367a0754b9b9a758e797874280d2a2d4a
Jai Sanghvi <85071341+jai-sanghvi@users.noreply.github.com>
2025-02-04 07:23:22 +0000

    Refactor gameover logic

--- src/modules/game-controller.js
+++ src/modules/game-controller.js
@@ -2,10 +2,10 @@ import Player from './player';
 import Ship from './ship';
 import { DOM } from './dom';
 
-const pOneShips = [new Ship(5), new Ship(4), new Ship(3), new Ship(3), new Ship(2)];
-const pTwoShips = [new Ship(5), new Ship(4), new Ship(3), new Ship(3), new Ship(2)];
 const randomizeButton = document.querySelector('#randomize');
 const startButton = document.querySelector('#start-game');
+const gameConfig = document.querySelector('#game-config');
+const gameStatus = document.querySelector('#game-status');
 let player1;
 let player2;
 const playerOneBoard = document.querySelector('#player1');
@@ -14,16 +14,21 @@ const playerTwoBoard = document.querySelector('#player2');
 
 document.addEventListener('DOMContentLoaded', createNewGame);
 randomizeButton.addEventListener('click', () => {
-  player1.gameboard.placeShipsRandomly(pOneShips);
+  const ships = player1.gameboard.ships;
+  player1.gameboard.ships = [];
+  player1.gameboard.placeShipsRandomly(ships);
   DOM.renderFirstBoard(player1, playerOneBoard);
 });
 startButton.addEventListener('click', () => {
   randomizeButton.style.display = 'none';
-  document.querySelector('#game-config').style.display = 'none';
+  gameConfig.style.display = 'none';
   startGame();
 })
 
 function createNewGame() {
+  const pOneShips = [new Ship(5), new Ship(4), new Ship(3), new Ship(3), new Ship(2)];
+  const pTwoShips = [new Ship(5), new Ship(4), new Ship(3), new Ship(3), new Ship(2)];
+
   player1 = new Player("real");
   player2 = new Player("computer");
 
@@ -35,29 +40,39 @@ function createNewGame() {
 }
 
 function startGame() {
-  playerTwoBoard.addEventListener('click', (e) => {
-    if (e.target.classList.contains("cell")) {
-      const x = e.target.dataset.x;
-      const y = e.target.dataset.y;
+  playerTwoBoard.addEventListener('click', playRound);
+}
 
-      if ((player2.gameboard.board[x][y] === 'hit') || (player2.gameboard.board[x][y] === 'miss')) return;
+function playRound(e) {
+  if (e.target.classList.contains("cell")) {
+    const x = e.target.dataset.x;
+    const y = e.target.dataset.y;
 
-      player2.gameboard.receiveAttack([x,y]);
-      DOM.renderSecondBoard(player2, playerTwoBoard);
+    if ((player2.gameboard.board[x][y] === 'hit') || (player2.gameboard.board[x][y] === 'miss')) return;
 
-      if (player2.gameboard.allSunk()) {
-        alert('game over -> you win!');
-        return;
-      }
+    player2.gameboard.receiveAttack([x,y]);
+    DOM.renderSecondBoard(player2, playerTwoBoard);
 
-      attackPlayerOne();
-
-      if (player1.gameboard.allSunk()) {
-        alert('game over -> you lose!');
-        return;
-      }
+    if (player2.gameboard.allSunk()) {
+      gameStatus.textContent = "GAME OVER - YOU WIN!";
+      gameConfig.style.display = 'grid';
+      randomizeButton.style.display = 'inline-block';
+      createNewGame();
+      startButton.textContent = "Play again";
+      return;
     }
-  })
+
+    attackPlayerOne();
+
+    if (player1.gameboard.allSunk()) {
+      gameStatus.textContent = "GAME OVER - YOU LOSE!";
+      gameConfig.style.display = 'grid';
+      randomizeButton.style.display = 'inline-block';
+      createNewGame();
+      startButton.textContent = "Play again";
+      return;
+    }
+  }
 }
 
 function attackPlayerOne() {
